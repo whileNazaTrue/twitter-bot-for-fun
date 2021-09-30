4fafa7a88a65f5d223d04da5f4c75efa2606fbd8
whileNazaTrue <84025828+whileNazaTrue@users.noreply.github.com>
2021-09-30 13:52:15 -0300

    Adding more query count

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -24,7 +24,7 @@ function getTweets(since_id){
     return new Promise((resolve,reject) => {
         let params = {
             q: "@BotSocialCredit",
-            count: 10,
+            count: 25,
         };
         if (since_id){
             params.since_id = since_id;
